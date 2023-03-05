04680442bd365c9d07dbdfdb7abcca32c9deb5e6
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-05 09:00:46 +0330

    Function: preparationTime

--- lasagna-master.js
+++ lasagna-master.js
@@ -12,3 +12,14 @@ function cookingStatus(time) {
 
 // console.log(cookingStatus(12));
 // console.log(cookingStatus());
+
+///---///
+
+// const layers = ["sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"];
+
+function preparationTime(layers, averagePreparationTime = 2) {
+  return layers.length * averagePreparationTime;
+}
+
+// console.log(preparationTime(layers, 3));
+// console.log(preparationTime(layers));
