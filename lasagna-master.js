98e2e0d5d0eeeb9302346730cbe03ea8187638e8
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-05 09:00:23 +0330

    Function: cookingStatus

--- lasagna-master.js
+++ lasagna-master.js
@@ -1,1 +1,14 @@
 // look back again 😢👏🤷‍♀️
+
+function cookingStatus(time) {
+  if (time === 0) {
+    return "Lasagna is done.";
+  } else if (time > 0) {
+    return "Not done, please wait.";
+  } else {
+    return "You forgot to set the timer.";
+  }
+}
+
+// console.log(cookingStatus(12));
+// console.log(cookingStatus());
